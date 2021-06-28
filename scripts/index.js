b6ed842a873d5140999441a86f94945b9d11198e
Ameena Hollyer <ameenahollyer@gmail.com>
2021-06-27 22:49:11 -0400

    javascript run

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+     let saveBtn = document.querySelector('#saveBtn');
+
+     let profileName = document.querySelector('.profile__name');
+     let profileCaption = document.querySelector('.profile__caption');
+
+     let formInput = document.querySelector(".popup__field");
+     let nameInput = formInput.querySelector("#name");
+     let captionInput = formInput.querySelector("#caption");
+
+     document.querySelector('.profile__square').addEventListener('click', function() {
+         document.querySelector('.popup').classList.add('popup_opened');
+     })
+
+     document.querySelector('.popup__close').addEventListener('click', function() {
+         document.querySelector('.popup').classList.remove('popup_opened');
+     })
+
+     saveBtn.addEventListener('click', function(event) {
+         nameInput.value = profileName.textContent;
+         captionInput.value = profileCaption.textContent;
+     });
+
+     saveBtn.addEventListener('click', function() {
+         profileName.textContent = nameInput.value;
+         profileTitle.textContent = titleInput.value;
+         closeModal();
+     })
--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-    document.querySelector('.profile__square').addEventListener('click', function() {
-        document.querySelector('.popup').classList.add('popup_opened');
-    })
-
-    document.querySelector('.popup__close').addEventListener('click', function() {
-        document.querySelector('.popup').classList.remove('popup_opened');
-    })
