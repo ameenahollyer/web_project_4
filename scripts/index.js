3f781b6218842349ce5906f4b10e7a01bcd0edd1
Ameena Hollyer <ameenahollyer@gmail.com>
2021-08-07 17:44:42 -0400

    pre-template

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,85 @@
-     let saveBtn = document.querySelector('#saveBtn');
+////////////////
+//Templates
+///////////////
 
-     let profileName = document.querySelector('.profile__name');
-     let profileCaption = document.querySelector('.profile__caption');
+const initialCards = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
 
-     let formInput = document.querySelector('.popup__form');
+//////////////////
+//Declarations
+/////////////////
+let saveBtn = document.querySelector('#saveBtn');
 
-     let formElement = document.querySelector('.popup__form');
+let profileName = document.querySelector('.profile__name');
+let profileCaption = document.querySelector('.profile__caption');
 
-     let popup = document.querySelector('.popup');
+let formInput = document.querySelector('.popup__form');
 
-     let profileSquare = document.querySelector('.profile__edit-button');
+let formElement = document.querySelector('.popup__form');
 
-     let popupClose = document.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
 
-     let nameInput = formInput.querySelector('#name');
+let profileSquare = document.querySelector('.profile__edit-button');
 
-     let captionInput = formInput.querySelector('#caption');
+let popupClose = document.querySelector('.popup__close');
 
-     function openForm() {
-         popup.classList.add('popup_opened');
-         nameInput.value = profileName.textContent;
-         captionInput.value = profileCaption.textContent;
-     }
+let nameInput = formInput.querySelector('#name');
+
+let captionInput = formInput.querySelector('#caption');
 
 
-     profileSquare.addEventListener('click', openForm);
 
-     popupClose.addEventListener('click', closePopup);
+/////////////////
+///functions
+/////////////////
 
-     function closePopup() {
-         popup.classList.remove('popup_opened');
-     }
+function openForm() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    captionInput.value = profileCaption.textContent;
+}
 
-     function handleFormSubmit(evt) {
-         evt.preventDefault();
-         profileName.textContent = nameInput.value;
-         profileCaption.textContent = captionInput.value;
-         closePopup()
-     }
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
 
-     formElement.addEventListener('submit', handleFormSubmit);
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileCaption.textContent = captionInput.value;
+    closePopup()
+}
+
+
+/////////////////
+///Event Handlers
+////////////////
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+profileSquare.addEventListener('click', openForm);
+
+popupClose.addEventListener('click', closePopup);
